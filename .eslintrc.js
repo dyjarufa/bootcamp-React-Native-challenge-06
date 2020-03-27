bdf9a669e8328a6ee4e419039d472b020a9785da
dyjarufa <jadyrufa@gmail.com>
2020-03-26 23:40:44 -0300

    Desafio App React Native

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react' //Integração do prettier com react
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly'
+  },
+  parser: 'babel-eslint', //O código entender as últimas versões do JS
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn', { // gero um warning para extensões que não são jsx ou js
+        extensions: ['.jsx', '.js']
+      }
+    ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/no-unused-state': 'off'
+  },
+};
---
+++ src/pages/User/index.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  OwnerAvatar,
+  Starred,
+  Info,
+  Title,
+  Author,
+  Loading,
+} from './styles';
+
+export default class User extends Component {
+  // console.tron.log(route);
+
+  // eslint-disable-next-line react/static-property-placement
+  static propTypes = {
+    route: PropTypes.shape({
+      params: PropTypes.shape({
+        user: PropTypes.shape({
+          login: PropTypes.string,
+          avatar: PropTypes.string,
+          name: PropTypes.string,
+          bio: PropTypes.string,
+        }),
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+    loading: true,
+    refreshing: false,
+    page: 1,
+  };
+
+  async componentDidMount() {
+    this.load();
+  }
+
+  load = async (page = 1) => {
+    const { stars } = this.state;
+    const { route } = this.props;
+    const { user } = route.params;
+
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page },
+    });
+
+    this.setState({
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      page,
+      loading: false,
+      refreshing: false,
+    });
+  };
+
+  refreshList = () => {
+    this.setState({ refreshing: true, stars: [] }, this.load);
+  };
+
+  loadMore = () => {
+    const { page } = this.state;
+
+    const nextPage = page + 1;
+
+    this.load(nextPage);
+  };
+
+  render() {
+    const { route } = this.props;
+    const { stars, loading, refreshing } = this.state;
+
+    const { user } = route.params;
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+        {loading ? (
+          <Loading />
+        ) : (
+          <Stars
+            data={stars}
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
+            keyExtractor={(star) => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
+      </Container>
+    );
+  }
+}
